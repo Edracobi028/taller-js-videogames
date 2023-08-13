d16236681282ce9f23d5fbd6878b7297f7ada65b
Eduardo Razo <esse_rios_e@hotmail.com>
2023-08-12 20:19:24 -0600

    Canvas responsive 4/24

--- game.js
+++ game.js
@@ -1,23 +1,49 @@
 console.log(maps);
 
-//la seleccionamos para empezar a utilizar por su id
-const canvas = document.querySelector('#game');
 
-//creamos variable para acceder a los metodos para dibujar
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game"); //la seleccionamos para empezar a utilizar por su id
+const game = canvas.getContext("2d"); //creamos variable para acceder a los metodos para dibujar
 
-//evento para que apenas cargue iniciar codigo
-window.addEventListener('load', startGame);
+//Definir variables
+let canvasSize;
+let elementsSize; //en base al ancho que toma, calcular los elemntos 10 x 10
+
+window.addEventListener("load", setCanvasSize); //evento ''load'' para que apenas cargue iniciar codigo
+window.addEventListener('resize', setCanvasSize); //evento ''resize'' para saber cuando cambian las medidas de nuestro navegador
 
 //crear una funcion para reutilizar, encapsular y organizar codigo inicial del juego
-function startGame() {
-    //el lugar donde iniciará nuestro trazo o area
-    //game.fillRect(0,0,100,100); //x,y,w,h Propiedad de trazo
-    //game.clearRect(50,50,50,50);
 
-    game.font = '25px Verdana'
-    game.fillStyle = 'Gray';
-    game.textAlign = 'center';
-    game.fillText('Platzi', 10, 25); //Propiedad de texto
+
+function setCanvasSize() {
+
+    //calculo para que nunca sobrepase las medidas y pierda la forma cuadrada
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8; //tome la medida del ancho
+    } else {
+        canvasSize = window.innerHeight * 0.8; //tome la medida del alto
+    }
+
+    //ancho responsivo
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame() {
+
+    console.log({
+        canvasSize,
+        elementsSize
+    });
+
+    game.font = elementsSize + 'px Verdana'; //Le damos tamaño de manera dinamica e indicamos tipo fuente
+    game.textAlign = 'end'; //posicionarlo al inicio de la 1ra linea
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize, elementsSize * i); //insertamos un emoji + posicion-x + posicion-y
+    }
 
 }
